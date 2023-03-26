123f1c41dfa893ff9dbc12d9501b6995653d53b8
Rubayed Ahmed <rubayedahmed04@gmail.com>
2023-03-27 00:26:29 +0600

    inital commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            This template is made by Rubayed Ahmed
+        </div>
+    );
+};
+
+export default Footer;
